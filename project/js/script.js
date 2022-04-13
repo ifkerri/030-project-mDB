a7d87edfcdb802a043e5def931363b6f3d58c6d8
ifkerri <ifkerri@mail.ru>
2022-04-13 21:59:05 +0500

    task #1

--- project/js/script.js
+++ project/js/script.js
@@ -12,15 +12,44 @@
 
 5) Добавить нумерацию выведенных фильмов */
 
-'use strict';
+//'use strict';
 
 const movieDB = {
     movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
+        'Логан',
+        'Лига справедливости',
+        'Ла-ла лэнд',
+        'Одержимость',
+        'Скотт Пилигрим против...'
     ]
 };
+
+const advertBlock = document.querySelector('.promo__adv'),
+      advertContent = advertBlock.querySelectorAll('img');
+      
+document.querySelector('.promo__adv-title').remove();
+advertContent.forEach((item, i) => {
+    item.remove();        
+});
+
+document.querySelector('.promo__genre').textContent = 'ДРАМА';
+document.querySelector('.promo__bg').style.background = 'url(\'../img/bg.jpg\')';
+
+movieDB.movies.sort();
+const promoInteractiveList = document.querySelector('.promo__interactive-list');
+promoInteractiveList.querySelectorAll('li').forEach(item => item.remove());
+for (let index = 0; index < movieDB.movies.length; index++) {
+    
+    const promoInteractiveListItem = document.createElement('li');
+    const promoInteractiveListItemDelete = document.createElement('div');
+
+    promoInteractiveListItem.setAttribute('class', 'promo__interactive-item');
+    promoInteractiveListItem.innerText = '' + (index + 1) + '. ' + movieDB.movies[index];
+    
+    promoInteractiveListItemDelete.setAttribute('class', 'delete');
+    
+    promoInteractiveListItem.append(promoInteractiveListItemDelete);
+    promoInteractiveList.append(promoInteractiveListItem);
+
+}
 
